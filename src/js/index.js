c7f6316eb8e795de91019be8e5447a5c76a2de58
JoseRegueiro17 <132882721+JoseRegueiro17@users.noreply.github.com>
2023-10-30 20:19:37 +0000

    cambiando elementos

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "../styles/index.css";
-
-// include your styles into the webpack bundle
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClock } from "@fortawesome/free-solid-svg-icons";
 import "../styles/index.css";
 
-//import your own components
-import Home from "./component/home.jsx";
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+const SimpleCounter = (props) =>{
+    return (
+        <div className="container clockcontainer">
+            <div className="clock"><FontAwesomeIcon icon={faClock}/></div>
+            <div>{props.digitSix % 10}</div>
+            <div>{props.digitFive % 10}</div>
+            <div>{props.digitFour % 10}</div>
+            <div>{props.digitThree % 10}</div>
+            <div>{props.digitTwo % 10}</div>
+            <div>{props.digitOne % 10}</div>
+        </div>
+    )
+};
+let counter = 0;
+setInterval(function(){
+    const six = Math.floor(counter/100000);
+    const five = Math.floor(counter/10000);
+    const four = Math.floor(counter/1000);
+    const three = Math.floor(counter/100);
+    const two = Math.floor(counter/10);
+    const one = Math.floor(counter/1);
+    counter ++;
+    console.log(one, two, three)
+    ReactDOM.render(
+    <SimpleCounter digitOne={one} digitTwo={two} digitThree={three} digitFour={four} digitFive={five} digitSix={six} />,
+
+    document.querySelector("#app"));},1000);
+
+
+
 
 
 
